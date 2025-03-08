5e9020150b6ca8cb60f3d6df7c5cc46303ac9f3e
Uoc Developer <vanssenlingga20@gmail.com>
2025-03-09 04:27:33 +0800

    Added the scripts in sdk.lua

--- Experiments Features/sdk.lua
+++ Experiments Features/sdk.lua
@@ -1,1 +1,15 @@
-Please wait
+
+-- Password Generator
+local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+math.randomseed(os.time())
+
+print("Enter the length of the password:")
+local length = tonumber(io.read())
+
+local password = ""
+for i = 1, length do
+    local randIndex = math.random(1, #chars)
+    password = password .. chars:sub(randIndex, randIndex)
+end
+
+print("Generated Password: " .. password)
