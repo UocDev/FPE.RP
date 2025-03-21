8e7c3a63ae61e003fc608ecb09b5ae1ffcb7a3c6
Kay17gntg <kayedit227@gmail.com>
2025-03-21 15:43:48 +0700

    Create userdatabase.lua

---
+++ Maps/userdatabase.lua
@@ -0,0 +1,49 @@
+local DataStoreService = game:GetService("DataStoreService")
+local playerDataStore = DataStoreService:GetDataStore("PlayerDatabase") -- Database name
+
+local function savePlayerData(player)
+    local key = "Player_" .. player.UserId -- Unique key for each player
+    local data = {
+        JoinDate = player:GetAttribute("JoinDate"),
+        PlayTime = player:GetAttribute("PlayTime"),
+        Coins = player:GetAttribute("Coins")
+    }
+    playerDataStore:SetAsync(key, data)
+end
+
+local function loadPlayerData(player)
+    local key = "Player_" .. player.UserId
+    local data = playerDataStore:GetAsync(key)
+
+    if data then
+        player:SetAttribute("JoinDate", data.JoinDate)
+        player:SetAttribute("PlayTime", data.PlayTime)
+        player:SetAttribute("Coins", data.Coins)
+    else
+        -- Set default values if new player
+        player:SetAttribute("JoinDate", os.date("%Y-%m-%d"))
+        player:SetAttribute("PlayTime", 0)
+        player:SetAttribute("Coins", 0)
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+    loadPlayerData(player)
+
+    -- Track playtime
+    while player do
+        local playTime = player:GetAttribute("PlayTime") or 0
+        player:SetAttribute("PlayTime", playTime + 1)
+        task.wait(60) -- Update every minute
+    end
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+    savePlayerData(player)
+end)
+
+game:BindToClose(function()
+    for _, player in pairs(game.Players:GetPlayers()) do
+        savePlayerData(player)
+    end
+end)
